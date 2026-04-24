4ac0968154e147919b29ee55970ebd113426c8f1
ddhanushd <dhanushdevaraj17@gmail.com>
2026-04-24 13:26:20 +0530

    ApaceKafka SpringBoot Consumer Demo

---
+++ src/main/java/com/kafka/kafkaconsumerexample/service/KafkaMessageListener.java
@@ -0,0 +1,31 @@
+package com.kafka.kafkaconsumerexample.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
+
+    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+    public void consumer1(String message) {
+        log.info("consumer1 consume the message {} ", message);
+    }
+
+    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+    public void consumer2(String message) {
+        log.info("consumer2 consume the message {} ", message);
+    }
+    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+    public void consumer3(String message) {
+        log.info("consumer3 consume the message {} ", message);
+    }
+
+    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+    public void consumer4(String message) {
+        log.info("consumer4 consume the message {} ", message);
+    }
+}
