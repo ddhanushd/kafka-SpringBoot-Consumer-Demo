cf44831ec5de5d4a384f4b0d53bd3f88ef4a2772
ddhanushd <dhanushdevaraj17@gmail.com>
2026-04-27 13:02:03 +0530

    Kafka consumer setup with JSON De-serialization working

--- src/main/java/com/kafka/kafkaconsumerexample/service/KafkaMessageListener.java
+++ src/main/java/com/kafka/kafkaconsumerexample/service/KafkaMessageListener.java
@@ -1,5 +1,6 @@
 package com.kafka.kafkaconsumerexample.service;
 
+import com.kafka.kafkaconsumerexample.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,22 +11,22 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
-    public void consumer1(String message) {
-        log.info("consumer1 consume the message {} ", message);
+    @KafkaListener(topics = "KafkaTopic-demo",groupId = "group-new")
+    public void consumeEvents(Customer customer) {
+        log.info("consumer consume the events {} ", customer.toString());
     }
 
-    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
-    public void consumer2(String message) {
-        log.info("consumer2 consume the message {} ", message);
-    }
-    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
-    public void consumer3(String message) {
-        log.info("consumer3 consume the message {} ", message);
-    }
-
-    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
-    public void consumer4(String message) {
-        log.info("consumer4 consume the message {} ", message);
-    }
+//    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+//    public void consumer2(String message) {
+//        log.info("consumer2 consume the message {} ", message);
+//    }
+//    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+//    public void consumer3(String message) {
+//        log.info("consumer3 consume the message {} ", message);
+//    }
+//
+//    @KafkaListener(topics = "KafkaTopic-demo-2", groupId = "group-new")
+//    public void consumer4(String message) {
+//        log.info("consumer4 consume the message {} ", message);
+//    }
 }
