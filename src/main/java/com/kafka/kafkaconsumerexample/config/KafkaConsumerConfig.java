81166c23cfcce6ec8096477a8122b817252489c1
ddhanushd <dhanushdevaraj17@gmail.com>
2026-04-29 12:31:12 +0530

    Kafka E2E Integration test with TestContainers- Consumer

--- src/main/java/com/kafka/kafkaconsumerexample/config/KafkaConsumerConfig.java
+++ src/main/java/com/kafka/kafkaconsumerexample/config/KafkaConsumerConfig.java
@@ -1,49 +1,49 @@
-package com.kafka.kafkaconsumerexample.config;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-
-import java.util.*;
-
-@Configuration
-public class KafkaConsumerConfig {
-    @Bean
-    public Map<String, Object> consumerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                JacksonJsonDeserializer.class);
-        // ✅ allow deserialization
-        props.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "*");
-
-        // 🔥 IMPORTANT FIXES
-        props.put(JacksonJsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-        props.put(JacksonJsonDeserializer.VALUE_DEFAULT_TYPE,
-                "com.kafka.kafkaconsumerexample.dto.Customer");
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<String,Object> consumerFactory(){
-        return new DefaultKafkaConsumerFactory<>(consumerConfig());
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
-}
+//package com.kafka.kafkaconsumerexample.config;
+//
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.apache.kafka.common.serialization.StringDeserializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.config.KafkaListenerContainerFactory;
+//import org.springframework.kafka.core.ConsumerFactory;
+//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+//import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+//import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+//
+//import java.util.*;
+//
+//@Configuration
+//public class KafkaConsumerConfig {
+//    @Bean
+//    public Map<String, Object> consumerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+//                "localhost:9092");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+//                StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+//                JacksonJsonDeserializer.class);
+//        // ✅ allow deserialization
+//        props.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "*");
+//
+//        // 🔥 IMPORTANT FIXES
+//        props.put(JacksonJsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+//        props.put(JacksonJsonDeserializer.VALUE_DEFAULT_TYPE,
+//                "com.kafka.kafkaconsumerexample.dto.Customer");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String,Object> consumerFactory(){
+//        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
+//}
--- src/test/java/com/kafka/kafkaconsumerexample/KafkaConsumerExampleApplicationTests.java
+++ src/test/java/com/kafka/kafkaconsumerexample/KafkaConsumerExampleApplicationTests.java
@@ -1,13 +1,54 @@
 package com.kafka.kafkaconsumerexample;
 
+import com.kafka.kafkaconsumerexample.dto.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import java.time.Duration;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@Slf4j
 class KafkaConsumerExampleApplicationTests {
 
+    @Container
+    static KafkaContainer kafka =
+            new KafkaContainer(
+                    DockerImageName.parse("apache/kafka:3.8.0")
+            );
+
+    @DynamicPropertySource
+    static void overridePropertiesInternal(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+
     @Test
-    void contextLoads() {
+    public void testConsumeEvents() {
+        log.info("testConsumeEvents method execution started...");
+        Customer customer = new Customer(263, "test user", "test@gmail.com", "564782542752");
+        kafkaTemplate.send("KafkaTopic-demo", customer);
+        log.info("testConsumeEvents method execution ended...");
+        await().pollInterval(Duration.ofSeconds(3)).atMost(10, SECONDS).untilAsserted(() -> {
+
+        });
     }
 
 }
